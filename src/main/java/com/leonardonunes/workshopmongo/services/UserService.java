c6b17243db73e0653b9d4df627b665837e90694c
leo123nunes <ti.userleo@hotmail.com>
2020-02-17 11:59:47 -0300

    Inserting new user with Post

--- src/main/java/com/leonardonunes/workshopmongo/services/UserService.java
+++ src/main/java/com/leonardonunes/workshopmongo/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.leonardonunes.workshopmongo.dto.UserDTO;
 import com.leonardonunes.workshopmongo.entities.User;
 import com.leonardonunes.workshopmongo.repositories.UserRepository;
 import com.leonardonunes.workshopmongo.services.exceptions.ObjectNotFoundException;
@@ -24,6 +25,15 @@ public class UserService {
 		if(user==null) {
 			throw new ObjectNotFoundException("Id not found.");
 		}
+		return user;
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		User user = new User(obj.getId(), obj.getName(), obj.getEmail());
 		return user;
 	}
 }
