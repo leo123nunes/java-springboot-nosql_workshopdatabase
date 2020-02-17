f80b6778f67247ccd7e1c93970f40d2c1d9e0707
leo123nunes <ti.userleo@hotmail.com>
2020-02-17 10:02:14 -0300

    Using DTO default to return users

--- src/main/java/com/leonardonunes/workshopmongo/resources/UserResource.java
+++ src/main/java/com/leonardonunes/workshopmongo/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.leonardonunes.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.leonardonunes.workshopmongo.dto.UserDTO;
 import com.leonardonunes.workshopmongo.entities.User;
 import com.leonardonunes.workshopmongo.services.UserService;
 
@@ -21,8 +21,9 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
