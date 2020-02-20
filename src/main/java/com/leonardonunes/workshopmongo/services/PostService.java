ef6c996943813a7f9099702446e9487a2a8590ad
leo123nunes <ti.userleo@hotmail.com>
2020-02-19 21:06:48 -0300

    Searching for the post by id

---
+++ src/main/java/com/leonardonunes/workshopmongo/services/PostService.java
@@ -0,0 +1,30 @@
+package com.leonardonunes.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leonardonunes.workshopmongo.entities.Post;
+import com.leonardonunes.workshopmongo.entities.User;
+import com.leonardonunes.workshopmongo.repositories.PostRepository;
+import com.leonardonunes.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public  List<Post> findAll(){
+		return repository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Post post = repository.findById(id).orElse(null);
+		if(post==null) {
+			throw new ObjectNotFoundException("Id not found.");
+		}
+		return post;
+	}
+}
