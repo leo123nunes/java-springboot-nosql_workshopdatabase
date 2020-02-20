fc1c468810a595829b0a98fc4c96ea5729b07285
leo123nunes <ti.userleo@hotmail.com>
2020-02-20 11:53:42 -0300

    Query with various criteria

--- src/main/java/com/leonardonunes/workshopmongo/resources/PostResource.java
+++ src/main/java/com/leonardonunes/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.leonardonunes.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,19 @@ public class PostResource {
 
 		text = URL.decodeParam(text);
 		List<Post> posts= service.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
+	}
+	
+	@RequestMapping(value = "/fullsearch",method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate){
+
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> posts= service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(posts);
 	}
 }
--- src/main/java/com/leonardonunes/workshopmongo/resources/util/URL.java
+++ src/main/java/com/leonardonunes/workshopmongo/resources/util/URL.java
@@ -2,13 +2,27 @@ package com.leonardonunes.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 	public static String decodeParam(String text) {
 		try {
 			return URLDecoder.decode(text, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
-			return "Post not found.";
+			return "";
+		}
+	}
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+		fmt.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try{
+			return fmt.parse(textDate);
+		}catch(ParseException e) {
+			return defaultValue;
 		}
 	}
 }
